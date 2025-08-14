f76b80e6e5f73e91c92bc468889a1d5c4f610eca
Deepak Kumar Yadav <dky422003@gmail.comm>
2025-08-14 19:06:36 +0530

    fix(auth) : fixed a missing import statement

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, signin, logout, deleteAccount } from '../controllers/auth.controller.js';
+import { signup, signin, logout, deleteAccount, getMe } from '../controllers/auth.controller.js';
 import { verifyUser } from '../utils/verifyUser.js';
 
 const router = express.Router();
